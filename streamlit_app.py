2d25512fe50a2667e091de30e552bae512d2eaca
Abhinav99 <juturnagaabhinavasai@gmail.com>
2026-09-16 11:35:20 +0530

    Update ingredient string handling and API request
    Fix ingredient string concatenation and add API call for watermelon.

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,10 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen
+        ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        # st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
